9e26da307bb175449526fe0e7bd993e0c3a160f7
GC <gouchao@soulapp.cn>
2020-01-06 17:09:16 +0800

    add MAPPER

--- gen/__init__.py
+++ gen/__init__.py
@@ -1,1 +1,15 @@
 #! -*- coding:utf8 -*-
+
+FIELD_MAPPER = {
+    "BIGINT": "BigInteger",
+    "INT": "Integer",
+    "INTEGER": "Integer",
+    "FLOAT": "",
+    "varchar": "String",
+    "text": "String",
+    "timestamp": "DateTime",
+    "typeint": "Integer",
+    "SMALLINT": "Integer",
+    "MEDIUMINT": "Integer",
+
+}
